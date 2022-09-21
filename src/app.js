df3972e21df2cf9ec3e3d6b68d2e9bcc8a02dfc6
Ferreira-Gerson <gersonferreira90@gmail.com>
2022-09-21 07:37:04 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let ext = [".com", ".net", ".org", ".us", ".io", ".pt", ".be"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  const domain = p =>
+    pronoun.flatMap(
+      adj.flatMap(a => noun.flatMap(n => ext.map(de => `${p}${a}${n}${e}`)))
+    );
+  console.log(domain);
 };
